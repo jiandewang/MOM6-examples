9d5f2c524ee66fd35c9d71b2a43a51e3fe7a4081
jiandewang <jiande.wang@noaa.gov>
2021-05-25 17:32:40 -0400

    add compiling script which reflect the code structure changes in MOM6

---
+++ compile-20210525.sh
@@ -0,0 +1,114 @@
+#!/bin/csh -f
+
+# download MOM6 code
+# git clone --recursive https://github.com/NOAA-GFDL/MOM6-examples.git MOM6-examples
+#
+set argv=(`getopt -u -o h -l platform:  --  $*`)
+while ("$argv[1]" != "--")
+    switch ($argv[1])
+        case --platform:
+                set platform = $argv[2]; shift argv; breaksw
+    endsw
+    shift argv
+end
+shift argv
+
+set BASEDIR=`pwd`
+set MACHINE_ID=${platform}  
+set COMPILE_OPTION=${MACHINE_ID}-intel.mk
+
+set compile_FMS=1
+set compile_ocean_only=1
+set compile_MOM6_SIS2=1
+###############################
+if ( ${compile_FMS} == 1 ) then 
+ echo "compile FMS library ..."
+ cd $BASEDIR
+ mkdir -p build/intel/shared/repro
+ cd build/intel/shared/repro
+ if ( -f path_names ) then
+  rm -f path_names
+ endif  
+
+ echo "generating file_paths ..."
+ ../../../../src/mkmf/bin/list_paths -l ../../../../src/FMS
+
+ echo "generating makefile ..."
+ ../../../../src/mkmf/bin/mkmf -t ../../../../src/mkmf/templates/${COMPILE_OPTION} -p libfms.a -c "-Duse_libMPI -Duse_netCDF" path_names
+
+ echo "compiling FMS library..."
+ make NETCDF=4 REPRO=1 libfms.a -j
+ cp libfms.a lib_FMS.a
+
+ echo "compiling FMS library done"
+
+endif 
+###############################################
+ if ( ${compile_ocean_only} == 1 ) then
+ echo "compile ocean only ..."
+ cd $BASEDIR
+ mkdir -p build/intel/ocean_only/repro
+ cd build/intel/ocean_only/repro
+ if ( -f path_names ) then
+  rm -f path_names
+ endif 
+
+ echo "generating file_paths ..."
+
+# ../../../../src/mkmf/bin/list_paths -l ./ ../../../../src/MOM6/{config_src/dynamic,config_src/solo_driver,config_src/external,src/{*,*/*}}
+ ../../../../src/mkmf/bin/list_paths -l ./ ../../../../src/MOM6/{config_src/infra/FMS1,config_src/memory/dynamic_nonsymmetric,config_src/drivers/solo_driver,config_src/external,src/{*,*/*}} 
+
+ echo "generating makefile ..."
+# ../../../../src/mkmf/bin/mkmf -t ../../../../src/mkmf/templates/${COMPILE_OPTION} -o '-I../../shared/repro' -p 'MOM6 -L../../shared/repro  -lfms' -c "-Duse_libMPI -Duse_netcdf -DSPMD" path_names
+
+ ../../../../src/mkmf/bin/mkmf -t ../../../../src/mkmf/templates/${COMPILE_OPTION} -o '-I../../shared/repro' -p MOM6 -l '-L../../shared/repro -lfms' path_names
+
+ echo "compiling MOM6 ocean only ..."
+ make NETCDF=4 REPRO=1 MOM6 -j
+
+# echo "generating libocean.a"
+# ar rv libocean.a *o
+ 
+ echo "compiling MOM6 ocean only done"
+
+endif 
+#######################################
+ if ( ${compile_MOM6_SIS2} == 1 ) then
+ echo "compiling MOM6-SIS2 ..."
+ cd $BASEDIR
+ mkdir -p build/intel/ice_ocean_SIS2/repro
+ cd build/intel/ice_ocean_SIS2/repro
+ if ( -f path_names ) then 
+  rm -f path_names
+ endif 
+
+ echo "generating file_paths ..."
+
+
+# ../../../../src/mkmf/bin/list_paths -l ./ ../../../../src/MOM6/config_src/{dynamic,coupled_driver,external} ../../../../src/MOM6/src/{*,*/*}/ ../../../../src/{atmos_null,coupler,land_null,ice_ocean_extras,ice_param,icebergs,SIS2,FMS/coupler,FMS/include}
+
+  ../../../../src/mkmf/bin/list_paths -l ./ ../../../../src/MOM6/config_src/{infra/FMS1,memory/dynamic_nonsymmetric,drivers/FMS_cap,external} ../../../../src/MOM6/src/{*,*/*}/ ../../../../src/{atmos_null,coupler,land_null,ice_param,icebergs,SIS2,FMS/coupler,FMS/include}
+
+ echo "generating makefile ..."
+
+# ../../../../src/mkmf/bin/mkmf -t ../../../../src/mkmf/templates/${COMPILE_OPTION} -o '-I../../shared/repro' -p MOM6 -l '-L../../shared/repro  -lfms' -c ' -Duse_AM3_physics -D_USE_LEGACY_LAND_' path_names
+  ../../../../src/mkmf/bin/mkmf -t ../../../../src/mkmf/templates/${COMPILE_OPTION} -o '-I../../shared/repro' -p MOM6 -l '-L../../shared/repro -lfms' -c '-Duse_AM3_physics -D_USE_LEGACY_LAND_' path_names
+
+ echo "compiling MOM6-SIS2 only ..."
+ make NETCDF=4 REPRO=1 MOM6 -j
+
+ echo "generating lib_ocean.a"
+ rm repro
+ ar rv lib_ocean.a *o
+
+ echo "compiling MOM6-SIS2 done"
+
+ # Install library and module files for NEMSAppbuilder
+ cd $BASEDIR
+ mkdir -p exec/${MACHINE_ID}/
+ # link to the library and module files
+ rm -rf exec/${MACHINE_ID}/lib_FMS exec/${MACHINE_ID}/lib_ocean
+ ln -s ${BASEDIR}/build/intel/shared/repro exec/${MACHINE_ID}/lib_FMS
+ ln -s ${BASEDIR}/build/intel/ice_ocean_SIS2/repro exec/${MACHINE_ID}/lib_ocean
+
+endif 
